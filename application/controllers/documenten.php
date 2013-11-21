d53c6c3f03c60bc95d188508d5d4ee8d085ab516
Vankerkom Kenneth <0930233@student.khlim.be>
2013-11-21 09:51:51 +0100

    Changes to documents
    Changes to upload files

--- application/controllers/documenten.php
+++ application/controllers/documenten.php
@@ -6,11 +6,45 @@
  */
 class Documenten_Controller extends CI_Controller
 {
-    function index()
+    
+    function __construct()
+	{
+		parent::__construct();
+		$this->load->helper(array('form', 'url'));
+	}
+
+	function index()
+	{
+		$this->load->view('upload_form', array('error' => ' ' ));
+	}
+
+	function do_upload()
+	{
+		$config['upload_path'] = './uploads/';
+		$config['allowed_types'] = 'gif|jpg|png';
+		$config['max_size']	= '100';
+		$config['max_width']  = '1024';
+		$config['max_height']  = '768';
+
+		$this->load->library('upload', $config);
+
+		if ( ! $this->upload->do_upload())
+		{
+			$error = array('error' => $this->upload->display_errors());
+
+			$this->load->view('upload_form', $error);
+		}
+		else
+		{
+			$data = array('upload_data' => $this->upload->data());
+
+			$this->load->view('upload_success', $data);
+		}
+	}
+   /* function index()
     {
         date_default_timezone_set("Europe/Brussels");
-        session_start();
-        connect_database();
+       
         
         //Check if logged on
         $blnLoggedOn = checkLogin();
@@ -29,7 +63,7 @@ class Documenten_Controller extends CI_Controller
             $this -> view -> assign("strContents", $this -> objError -> getPageData());
         }
         $this -> view -> display("index_view");
-    }
+    }*/
     
 }
 ?>
