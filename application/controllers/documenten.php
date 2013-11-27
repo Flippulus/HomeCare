df441948b7e6e82d1c0beb66f8ad7739225d76d5
Vankerkom Kenneth <0930233@student.khlim.be>
2013-11-27 15:05:02 +0100

    CI
    Document & Upload changes

--- application/controllers/documenten.php
+++ application/controllers/documenten.php
@@ -6,64 +6,24 @@
  */
 class Documenten extends CI_Controller
 {
-    
-    function __construct()
-	{
-		parent::__construct();
-		$this->load->helper(array('form', 'url'));
-	}
-
-	function index()
-	{
-		$this->load->view('upload_form', array('error' => ' ' ));
-	}
-
-	function do_upload()
-	{
-		$config['upload_path'] = './uploads/';
-		$config['allowed_types'] = 'gif|jpg|png';
-		$config['max_size']	= '100';
-		$config['max_width']  = '1024';
-		$config['max_height']  = '768';
-
-		$this->load->library('upload', $config);
-
-		if ( ! $this->upload->do_upload())
-		{
-			$error = array('error' => $this->upload->display_errors());
-
-			$this->load->view('upload_form', $error);
-		}
-		else
-		{
-			$data = array('upload_data' => $this->upload->data());
-
-			$this->load->view('upload_success', $data);
-		}
-	}
-   /* function index()
+  
+    public function file_upload_demo()
     {
-        date_default_timezone_set("Europe/Brussels");
-       
+        try
+        {
+            if($this->input->post("submit")){        
+                $this->load->library("application/uploader");
+                $this->uploader->do_upload();
+            }
+            return $this->view();
+        }
         
-        //Check if logged on
-        $blnLoggedOn = checkLogin();
-        if($blnLoggedOn == true)
+        catch(Exception $err)
         {
-            createPageStart("HomeCare", array());
-            $this -> load -> model("MenuItems_Model", "objMenuItems");
-            $arrMainMenuItems = $this -> objMenuItems -> getMainMenuItems();
-            $this -> load -> model("Documenten_Model", "objModel");
-            //Loading the model so the page contents can be created and given to the view
-            $this -> view -> assign("strContents", $this -> objModel -> getPageData($arrMainMenuItems, $blnLoggedOn));
+            log_message("error",$err->getMessage());
+            return show_error($err->getMessage());
         }
-        else
-        {
-            $this -> load -> model("NoEntry_Model", "objError");
-            $this -> view -> assign("strContents", $this -> objError -> getPageData());
-        }
-        $this -> view -> display("index_view");
-    }*/
+    }
     
 }
 ?>
--- application/views/upload_form.php
+++ application/views/upload_form.php
@@ -6,15 +6,18 @@
 <body>
 
 
-<?php echo form_open_multipart('upload/do_upload');?>
-
-<input type="file" name="userfile" size="20" />
-
-<br /><br />
-
-<input type="submit" value="upload" />
-
-</form>
-
+        <form action="" method="POST" enctype="multipart/form-data" >
+            Select File To Upload:<br />
+            <input type="file" name="userfile" multiple="multiple"  />
+            
+            <input type="submit" name="submit" value="Upload" class="btn btn-success" />
+        </form>
+ 
+        <?php {if isset($uploaded_file)}
+            {foreach from=$uploaded_file key=name item=value}
+                {$name} : {$value}
+                <br />
+            {/foreach}    
+        {/if} ?>
 </body>
 </html>
